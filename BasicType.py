02d9610dcb67c5b91e6b55eea77fe9e21dda7e60
Anvesh Vs <anveshtracks@gmail.com>
2017-07-15 17:24:32 +0530

    Python basic types for data science.

---
+++ BasicType.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 18 08:22:16 2017
+
+@author: Anvesh
+"""
+
+x = 10
+print(type(x))
+x = 10.5
+print(type(x))
+name1="abc"
+name2='xyz'
+print(type(name1))
+print(type(name2))
+y = 10+15j
+print(type(y))
+z= True
+print(type(z))
+
+
+
