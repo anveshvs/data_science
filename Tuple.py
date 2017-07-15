76bf3fd80425ce5f4ef51c4f90e2e410654876b6
Anvesh Vs <anveshtracks@gmail.com>
2017-07-15 17:33:25 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+tuple1=(10,20,30)
+type(tuple1)
+tuple1[0]
+tuple1[0:2]
+tuple1[:3]
+
+# Tuple is an immutable collection
+tuple1[0] =50
+tuple1[0]
+tuple2 = (10,True,'abc')
+tuple2[0]
+tuple2[1]
+for x in tuple2:
+    print(x)
