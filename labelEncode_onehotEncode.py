90d300ecd952da6795ec1881681aa88dfa60c578
Anvesh Vs <anveshtracks@gmail.com>
2017-07-16 15:57:05 +0530

    Label encoding and onehot encoding

---
+++ labelEncode_onehotEncode.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 16 09:20:30 2017
+
+@author: Anvesh
+"""
+
+from numpy import array
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+Example:1
+# define example
+data = ['cold', 'cold', 'warm', 'cold', 'hot', 'hot', 'warm', 'cold', 'warm', 'hot']
+values = array(data)
+print(values)
+# integer encode
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(values)
+print(integer_encoded)
+# binary encode
+onehot_encoder = OneHotEncoder(sparse=False)
+integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+print(onehot_encoded)
+# invert first example
+inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
+print(inverted)
+
+Example:2
+#One Hot Encode with Keras
+## Need keras jar.................for this
+from numpy import array
+from numpy import argmax
+from keras.utils import to_categorical
+# define example
+data = [1, 3, 2, 0, 3, 2, 2, 1, 0, 1]
+data = array(data)
+print(data)
+# one hot encode
+encoded = to_categorical(data)
+print(encoded)
+# invert encoding
+inverted = argmax(encoded[0])
+print(inverted)
+
+
+titanic_train['Pclass'] = titanic_train.Pclass.astype('category',categories = [1,2,3],ordered = True)
